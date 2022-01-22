2bd91c51d87895e57be4a4d9e28a6fc1ecfd6f6f
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-22 22:26:57 +0530

    Update button callback function name

--- script/main.js
+++ script/main.js
@@ -5,7 +5,7 @@ const message = document.querySelector("#message-box");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkBtn.addEventListener("click", () => {
+checkBtn.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
@@ -21,14 +21,14 @@ checkBtn.addEventListener("click", () => {
 
 //Helper functions -
 function calculateChange(cashToBeReturned) {
-  for(let i = 0; i < notes.length; i++){
+  for (let i = 0; i < notes.length; i++) {
     let numberOfNotes = Math.trunc(cashToBeReturned / notes[i]);
     cashToBeReturned %= notes[i];
     noOfNotes[i].textContent = numberOfNotes;
   }
 }
 
-function hideMessage(){
+function hideMessage() {
   message.style.display = "none";
 }
 
@@ -37,4 +37,3 @@ function showMessage(text) {
   message.textContent = text;
   message.style.color = "red";
 }
-
