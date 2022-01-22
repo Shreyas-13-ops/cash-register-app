0205bd1e375a67596429886c16dbe9b9b2069311
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-22 18:44:48 +0530

    Amount validation JS

--- script/main.js
+++ script/main.js
@@ -1,1 +1,34 @@
-console.log("Hello console");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#btn-check");
+const message = document.querySelector("#message-box");
+const notes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkBtn.addEventListener("click", () => {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      let cashToBeReturned = cashGiven.value - billAmount.value;
+      calculateChangeAmt(cashToBeReturned);
+    } else {
+      showMessage("Cash amount is less than -- 'Bill Amount'.");
+    }
+  } else {
+    showMessage("Invalid -- 'Bill Amount', please check again.");
+  }
+});
+
+//Helper functions -
+function hideMessage(){
+  message.style.display = "none";
+}
+
+function showMessage(text) {
+  message.style.display = "block";
+  message.textContent = text;
+  message.style.color = "red";
+}
+
+function calculateChangeAmt(amount) {
+
+}
