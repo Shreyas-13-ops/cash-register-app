666d2cb59bf6d91a554a3afd0769a7948828076b
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-22 19:19:10 +0530

    Add calculate logic

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBtn = document.querySelector("#btn-check");
 const message = document.querySelector("#message-box");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkBtn.addEventListener("click", () => {
@@ -9,16 +10,24 @@ checkBtn.addEventListener("click", () => {
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
       let cashToBeReturned = cashGiven.value - billAmount.value;
-      calculateChangeAmt(cashToBeReturned);
+      calculateChange(cashToBeReturned);
     } else {
-      showMessage("Cash amount is less than -- 'Bill Amount'.");
+      showMessage("Cash amount is less than 'Bill Amount'.");
     }
   } else {
-    showMessage("Invalid -- 'Bill Amount', please check again.");
+    showMessage("Invalid 'Bill Amount', please check again.");
   }
 });
 
 //Helper functions -
+function calculateChange(cashToBeReturned) {
+  for(let i = 0; i < notes.length; i++){
+    let numberOfNotes = Math.trunc(cashToBeReturned / notes[i]);
+    cashToBeReturned %= notes[i];
+    noOfNotes[i].textContent = numberOfNotes;
+  }
+}
+
 function hideMessage(){
   message.style.display = "none";
 }
@@ -29,6 +38,3 @@ function showMessage(text) {
   message.style.color = "red";
 }
 
-function calculateChangeAmt(amount) {
-
-}
